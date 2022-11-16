8186406a61572f8ebb4f867c4d67534f8ab0199a
EvanWard29 <Evan29Ward@gmail.com>
2022-11-16 19:13:42 +0000

    Hand Animations - need to fix the 3 fingers resetting after grabbing an object

---
+++ Assets/My Stuff/Scripts/HandPresence.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;    
+    private InputDevice targetDevice;
+    public Animator animator;
+    public GameObject thisHand;
+
+    //these are the same values that are used within the animator for the hand model
+    public float indexValue;
+    public float thumbValue;
+    public float threeFingersValue;
+
+    public float thumbMoveSpeed = 0.1f;
+
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+    }
+
+    void UpdateHandAnimation()
+    {
+        //this will set the indexValue to how much you are pressing the trigger button so it knows how much to animate the hand
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out indexValue);
+        targetDevice.TryGetFeatureValue(CommonUsages.grip, out threeFingersValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryTouch, out bool primaryTouched);
+        targetDevice.TryGetFeatureValue(CommonUsages.secondaryTouch, out bool secondaryTouched);
+
+        //if you're touching the buttons on the controller it'll detect thumb input
+        if (primaryTouched || secondaryTouched)
+        {
+            thumbValue += thumbMoveSpeed;
+        }
+        else
+        {
+            thumbValue -= thumbMoveSpeed;
+        }
+
+        thumbValue = Mathf.Clamp(thumbValue, 0, 1);
+
+        //setting the index float on the animator to the value of how much you have pressed it
+        animator.SetFloat("Index", indexValue);
+        animator.SetFloat("ThreeFingers", threeFingersValue);
+        animator.SetFloat("Thumb", thumbValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            UpdateHandAnimation();
+        }
+    }
+}
