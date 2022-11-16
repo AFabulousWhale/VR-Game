93179b42c9c7bfbba877358ee73e9b0b5fd28613
EvanWard29 <Evan29Ward@gmail.com>
2022-11-16 17:58:19 +0000

    Can win and lose game

---
+++ Assets/My Stuff/Scripts/WinLose.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLose : MonoBehaviour
+{
+    public Timer timer;
+
+    bool atEnd;
+
+    void Update()
+    {
+        if (timer.time <= 0)
+        {
+            Debug.Log("You lose");
+            SceneManager.LoadScene("Lose");
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("You won");
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
